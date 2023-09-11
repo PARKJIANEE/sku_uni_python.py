95f73ca61ccd4adb06e8cb273a900512720bf1ec
PARKJIANEE <143483610+PARKJIANEE@users.noreply.github.com>
2023-09-11 14:08:48 +0900

    Add files via upload

---
+++ 20230911.py
@@ -0,0 +1,56 @@
+#import keyword
+#print(keyword.kwlist)
+
+#import builtins
+#print(dir(builtins))
+
+#base=3 #밑변
+#height=40 #높이
+#area=base*height/2 #삼각형 면적 공식
+#print('삼각형의 면적 = ',area)
+
+#True=3 #cannot assign to True 오류 나옴. >> true=3 으로 입력
+#7Eleven=100 #invalid syntax 오류 나옴. >> 변수 이름 숫자로 안됨. >> Eleven=100 으로 입력.
+#speed=100
+#one+two=3 #cannot assign to operator 오류 나옴. >> '+' 기호 안됨.
+#원주율=3.14
+#for=20 #invalid syntax 오류 나옴. >> 예약어 안됨.
+
+# no=None
+# print(no)
+# print(type(no)) #<class>형태로 출력.
+# b1=True #b=bool
+# b2=False
+# print(type(b1),b1)
+# print(type(b2),b2)
+
+# a=5
+# a=-178
+# a=0
+# print(a)
+# print(type(a))
+# Oct_a=0o177
+# print(int(Oct_a))
+# print(type(Oct_a))
+# Oct_b=0o251
+# print(int(Oct_b))
+# hex_a=0x8ff
+# print(int(hex_a))
+# hex_b=0x10
+# print(int(hex_b))
+# print(a)
+
+# a=1.2
+# b=-3.45
+# print(a,type(a))
+# print(b,type(b))
+# c=4.24E10
+# d=4.24e-2
+# print(c)
+# print(d)
+
+# e=2-3j
+# print(e.real) #e의 실수값
+# print(e.imag) #e의 허수값
+# print(type(e))
+
